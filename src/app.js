5118f2859dae71cba7cb233c872cc4fe26b4dac2
dilek sinanoglu <dilek.sinanoglu@ddtech.com.tr>
2021-10-24 00:09:19 +0300

    first course

---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log("dilek")
+
+let konutKredileri = ["emlak kredisi", "konut kredisi","kamu konut kredisi"]
+
+for(let i=0; i<konutKredileri.length; i++){
+    console.log(konutKredileri[i])
+}
